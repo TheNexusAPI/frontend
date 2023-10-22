f34e129759424f2f2a8eb8426d4c1efa7b1d8be1
neelthepatel <neelthepatel8@gmail.com>
2023-10-21 20:57:49 -0400

    Add background to navbar when scrolling

--- components/Navigation/Navbar.js
+++ components/Navigation/Navbar.js
@@ -1,11 +1,37 @@
-import React from "react";
+"use client";
+
+import React, { useState, useEffect } from "react";
 import NexusAPILogo from "../NexusAPI_Logo/NexusAPILogo";
 import LoginIcon from "../Icons/LoginIcon/LoginIcon";
 
 const Navbar = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 0) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+  }, []);
+
   return (
-    <nav className="absolute top-0 w-full">
-      <div className="flex flex-row justify-between items-center bg-black h-16 text-white drop-shadow-2xl px-10 bg-transparent">
+    <nav
+      className={
+        scrolled
+          ? "fixed top-0 w-full bg-black h-16 text-white drop-shadow-2xl px-10 z-50 shadow-2xl"
+          : "fixed top-0 w-full bg-transparent h-16 text-white drop-shadow-2xl px-10 z-50"
+      }
+    >
+      <div className="flex flex-row justify-between items-center">
         <NexusAPILogo />
         <div className="flex flex-row justify-between items-center gap-10 text-md mr-16">
           <div className="hover:text-purple-400 cursor-pointer transition-all duration-300">
